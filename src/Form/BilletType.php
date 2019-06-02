f47932233c9723be9d853fece34a121355d3a672
Bastien Gauthier <bastien_gau@hotmail.fr>
2019-06-02 22:19:05 +0200

    Menu burger fonctionnel

--- src/Form/BilletType.php
+++ src/Form/BilletType.php
@@ -6,7 +6,6 @@ use App\Entity\Billet;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
